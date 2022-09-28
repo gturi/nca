c6acf311a8f5b27c9b0776d695294e928936a84a
gventurini <giacomo.venturini3@gmail.com>
2022-09-28 23:51:47 +0200

    add parsing of alias options

--- src/util/model-mapper.ts
+++ src/util/model-mapper.ts
@@ -1,7 +1,8 @@
-import yargs, { CommandModule, describe } from 'yargs';
+import yargs, { CommandModule, describe, InferredOptionType, Options } from 'yargs';
 import { Alias } from '../model/alias';
 import shelljs from 'shelljs';
 import { CommandType } from '../model/command-type';
+import { AliasOption } from '../model/alias-option';
 
 export class ModelMapper {
 
@@ -9,6 +10,15 @@ export class ModelMapper {
     return {
       command: alias.name,
       describe: alias.description,
+      builder: yargs => {
+        if (alias.options) {
+          return alias.options.reduce(
+            (_prev, aliasOption) => ModelMapper.mapOptions(yargs, aliasOption),
+            ModelMapper.emptyBuilder()
+          )
+        }
+        return ModelMapper.emptyBuilder();
+      },
       handler: args => {
         if (CommandType.Function === alias.commandType) {
           ModelMapper.functionRunner(args, alias.command);
@@ -17,6 +27,19 @@ export class ModelMapper {
         }
       }
     }
+  }
+
+  private static mapOptions<T = {}>(yargs: yargs.Argv<T>, aliasOption: AliasOption): yargs.Argv<T & { [key in string]: InferredOptionType<Options> }> {
+    const opt: Options = {
+      alias: aliasOption.alternativeName,
+      type: aliasOption.optionType,
+      default: aliasOption.defaultValue
+    }
+    return yargs.option(aliasOption.name, opt);
+  }
+
+  private static emptyBuilder<T = {}>(): yargs.Argv<T & { [key in string]: InferredOptionType<Options> }> {
+    return {} as yargs.Argv<T & { [key in string]: InferredOptionType<Options> }>
   }
 
   private static functionRunner(args: yargs.ArgumentsCamelCase<{}>, code: string) {

2d291e492af1bde59342ec991c66e19a80edbdb1
gventurini <giacomo.venturini3@gmail.com>
2022-09-29 23:55:18 +0200

    modularize code and apply better naming convention

--- src/util/model-mapper.ts
+++ src/util/model-mapper.ts
@@ -13,61 +13,77 @@ export class ModelMapper {
     return {
       command: alias.name,
       describe: alias.description,
-      builder: yargs => {
-        var builder = ModelMapper.emptyBuilder();
-
-        if (alias.options) {
-          alias.options.forEach(aliasOption => {
-            ModelMapper.mapOptions(yargs, aliasOption);
-          });
-        }
-
-        if (alias.positionalArguments) {
-          alias.positionalArguments.forEach(positionalArgument => {
-            ModelMapper.mapPositional(yargs, positionalArgument);
-          });
-        }
-
-        if (alias.subAliases) {
-          alias.subAliases.forEach(subAlias => {
-            yargs.command(ModelMapper.mapAlias(subAlias));
-          });
-        }
-
-        return builder;
-      },
-      handler: args => {
-        if (alias.command !== '') {
-          if (CommandType.Function === alias.commandType) {
-            ModelMapper.functionRunner(args, alias.command);
-          } else {
-            shelljs.exec(alias.command);
-          }
-        }
-      }
+      builder: yargs => ModelMapper.getBuilder(yargs, alias),
+      handler: args => ModelMapper.getHandler(args, alias)
     }
+  }
+
+  private static getBuilder<T = {}>(yargs: yargs.Argv<T>, alias: Alias): ArgvBuilder<T> {
+    var builder = ModelMapper.emptyBuilder<T>();
+
+    ModelMapper.buildOptions(yargs, alias.options);
+
+    ModelMapper.buildPositionalArguments(yargs, alias.positionalArguments);
+
+    ModelMapper.buildSubAliases(yargs, alias.subAliases);
+
+    return builder;
   }
 
   private static emptyBuilder<T = {}>(): ArgvBuilder<T> {
     return {} as ArgvBuilder<T>;
   }
 
-  private static mapOptions<T = {}>(yargs: yargs.Argv<T>, aliasOption: AliasOption): ArgvBuilder<T> {
+  private static buildOptions<T = {}>(yargs: yargs.Argv<T>, aliasOptions?: AliasOption[]) {
+    if (aliasOptions) {
+      aliasOptions.forEach(aliasOption => {
+        ModelMapper.buildOption(yargs, aliasOption);
+      });
+    }
+  }
+
+  private static buildOption<T = {}>(yargs: yargs.Argv<T>, aliasOption: AliasOption) {
     const opt: Options = {
       alias: aliasOption.alternativeName,
       type: aliasOption.optionType,
       default: aliasOption.defaultValue
     }
-    return yargs.option(aliasOption.name, opt);
+    yargs.option(aliasOption.name, opt);
   }
 
-  private static mapPositional<T = {}>(yargs: yargs.Argv<T>, positionalArgument: AliasPositionalArgument): ArgvBuilder<T> {
-    return yargs.positional(positionalArgument.name, {
+  private static buildPositionalArguments<T = {}>(yargs: yargs.Argv<T>, positionalArguments?: AliasPositionalArgument[]) {
+    if (positionalArguments) {
+      positionalArguments.forEach(positionalArgument => {
+        ModelMapper.buildPositional(yargs, positionalArgument);
+      });
+    }
+  }
+
+  private static buildPositional<T = {}>(yargs: yargs.Argv<T>, positionalArgument: AliasPositionalArgument) {
+    yargs.positional(positionalArgument.name, {
       describe: positionalArgument.description,
       type: positionalArgument.type,
       default: positionalArgument.defaultValue,
       required: positionalArgument.required,
     });
+  }
+
+  private static buildSubAliases<T = {}>(yargs: yargs.Argv<T>, subAliases?: Alias[]) {
+    if (subAliases) {
+      subAliases.forEach(subAlias => {
+        yargs.command(ModelMapper.mapAlias(subAlias));
+      });
+    }
+  }
+
+  private static getHandler(args: yargs.ArgumentsCamelCase<{}>, alias: Alias) {
+    if (alias.command !== '') {
+      if (CommandType.Function === alias.commandType) {
+        ModelMapper.functionRunner(args, alias.command);
+      } else {
+        shelljs.exec(alias.command);
+      }
+    }
   }
 
   private static functionRunner(args: yargs.ArgumentsCamelCase<{}>, code: string) {

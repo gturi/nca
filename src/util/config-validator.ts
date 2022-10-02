63a8f68f5a29309db607ef951e76c7776b45f3fd
gventurini <giacomo.venturini3@gmail.com>
2022-10-02 16:47:14 +0200

    add option and positional arguments validation

--- src/util/config-validator.ts
+++ src/util/config-validator.ts
@@ -7,20 +7,52 @@ export class ConfigValidator {
     ConfigValidator.throwErrorIfAliasIsDefined(configPath, config, 'completion');
     ConfigValidator.throwErrorIfAliasIsDefined(configPath, config, 'config');
   }
-  
-  private static throwErrorIfAliasIsDefined(configPath: string, config: Config, aliasName:string) {
+
+  private static throwErrorIfAliasIsDefined(configPath: string, config: Config, aliasName: string) {
     if (config.aliases?.some(alias => alias.name === aliasName)) {
       throw new Error(`${configPath}: You can not define an alias called '${aliasName}' since it is a special keyword`);
     }
   }
 
-  static validateAliases(configs: Alias[]) {
-    const aliasNames = configs.map(alias => alias.name);
+  static validateAliases(aliases: Alias[]) {
+    const aliasNames = aliases.map(alias => alias.name);
 
     const duplicates = aliasNames.filter(ConfigValidator.findDuplicates);
 
     if (duplicates.length > 0) {
       throw new Error(`Multiple aliases has been defined with the same name: [${duplicates}]`);
+    }
+
+    aliases.forEach(alias => {
+      ConfigValidator.validateOptions(alias);
+      ConfigValidator.validatePositionalArguments(alias);
+      if (alias.subAliases) {
+        ConfigValidator.validateAliases(alias.subAliases);
+      }
+    });
+  }
+
+  private static validateOptions(alias: Alias) {
+    if (alias.options) {
+      const optionNames = alias.options.map(option => option.name);
+
+      const duplicates = optionNames.filter(ConfigValidator.findDuplicates);
+
+      if (duplicates.length > 0) {
+        throw new Error(`${alias.name}: multiple option has been defined with the same name: [${duplicates}]`);
+      }
+    }
+  }
+
+  private static validatePositionalArguments(alias: Alias) {
+    if (alias.positionalArguments) {
+      const positionalArgumentNames = alias.positionalArguments.map(positionalArgument => positionalArgument.name);
+
+      const duplicates = positionalArgumentNames.filter(ConfigValidator.findDuplicates);
+
+      if (duplicates.length > 0) {
+        throw new Error(`${alias.name}: multiple positional arguments has been defined with the same name: [${duplicates}]`);
+      }
     }
   }
 

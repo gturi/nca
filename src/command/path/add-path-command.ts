dc152cbe16f3afbbfb6c199871838965a2879ca4
gventurini <giacomo.venturini3@gmail.com>
2022-10-09 23:27:47 +0200

    add handler for add path

--- src/command/path/add-path-command.ts
+++ src/command/path/add-path-command.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import yaml from 'js-yaml';
 import yargs from "yargs";
 import { Config } from "../../model/config";
 import { AnyObj, ArgvBuilder } from "../../util/custom-types";
@@ -28,5 +30,25 @@ export class AddPathCommand extends Command {
     });
 
     return super.getBuilder<T>(yargs);
+  }
+
+  protected getHandler<T = AnyObj>(args: yargs.ArgumentsCamelCase<T>): void {
+    const configPath = args.f as string;
+    const configPathToAdd = args.configPath as string;
+
+    if (!fs.existsSync(configPath)) {
+      throw new Error(`Config file not found: ${configPath}`);
+    }
+
+    const data = fs.readFileSync(configPath, 'utf8');
+    const config: Config = yaml.load(data) ?? {};
+
+    if (config.includePaths) {
+      config.includePaths.push(configPathToAdd);
+    } else {
+      config.includePaths = [configPathToAdd];
+    }
+
+    fs.writeFileSync(configPath, yaml.dump(config), { encoding: 'utf8' });
   }
 }

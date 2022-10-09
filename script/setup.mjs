5389431e3c840bbf1db46597a013af2a05a2af4b
gventurini <giacomo.venturini3@gmail.com>
2022-10-09 13:01:02 +0200

    add preinstall setup script

---
+++ script/setup.mjs
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import os from "os";
+import path from 'path';
+
+console.log('Running setup script');
+
+const configDir = path.join(os.homedir(), '.nca');
+const configPath = path.join(configDir, 'config.yml');
+
+if (fs.existsSync(configPath)) {
+  console.log('Config file already exists, nothing to do.');
+} else {
+  fs.mkdirSync(configDir, { recursive: true });
+  fs.writeFileSync(configPath, '');
+  console.log('Config file created successfully.');
+}
+
+console.log('Setup script ended successfully');

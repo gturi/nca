b4f7e2a4183b87306acae93a18359c3edf178df8
gventurini <giacomo.venturini3@gmail.com>
2022-10-09 14:36:41 +0200

    uniform logs format

--- script/setup.mjs
+++ script/setup.mjs
@@ -8,11 +8,11 @@ const configDir = path.join(os.homedir(), '.nca');
 const configPath = path.join(configDir, 'config.yml');
 
 if (fs.existsSync(configPath)) {
-  console.log('Config file already exists, nothing to do.');
+  console.log('Config file already exists, nothing to do');
 } else {
   fs.mkdirSync(configDir, { recursive: true });
   fs.writeFileSync(configPath, '');
-  console.log('Config file created successfully.');
+  console.log('Config file created successfully');
 }
 
 console.log('Setup script ended successfully');

6035763afc62982e42b20fac7a90e28f2deb937a
gventurini <giacomo.venturini3@gmail.com>
2022-10-12 22:35:20 +0200

    add workflow that publishes library dev version

---
+++ .github/script/create-develop-version.mjs
@@ -0,0 +1,18 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const packageJson = require('../../package.json');
+
+const currentVersion = packageJson.version;
+
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth() + 1;
+const day = currentDate.getDate();
+const hours = currentDate.getHours();
+const minutes = currentDate.getMinutes();
+const seconds = currentDate.getSeconds();
+
+const currentDateFormat = `${year}.${month}.${day}-${hours}.${minutes}.${seconds}`;
+
+console.log(`${currentVersion}-dev-${currentDateFormat}`);

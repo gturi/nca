5380c0c4dd6dc392863bd65b3100677945b6203a
gventurini <giacomo.venturini3@gmail.com>
2022-10-17 23:13:29 +0200

    add utility function that throws an exception if exec exit code is not 0

--- src/mapper/yargs-handler-builder.ts
+++ src/mapper/yargs-handler-builder.ts
@@ -1,7 +1,7 @@
 import yargs from "yargs";
 import { Alias } from "../model/alias";
 import { CommandType } from "../model/command-type";
-import shelljs from 'shelljs';
+import shelljs, { ShellString } from 'shelljs';
 import { StringUtils } from "../util/string-utils";
 import { AnyObj } from "../util/custom-types";
 
@@ -22,8 +22,17 @@ export class YargsHandlerBuilder {
       "use strict;"
       const args = arguments[0];
       const shelljs = arguments[1];
+      const safeExec = arguments[2];
       ${code}
     `);
-    fun(args, shelljs);
+    fun(args, shelljs, this.safeExec);
+  }
+
+  private static safeExec(command: string, errorMessage?: string): ShellString {
+    const result = shelljs.exec(command);
+    if (result.code !== 0) {
+      throw new Error(errorMessage ?? 'Command returned non 0 error code');
+    }
+    return result;
   }
 }

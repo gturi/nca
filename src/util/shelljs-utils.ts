d6be7929fa95ab25e9d2d30b11b39bc9bd92293a
gventurini <giacomo.venturini3@gmail.com>
2023-02-05 22:23:22 +0100

    invert if statement

--- src/util/shelljs-utils.ts
+++ src/util/shelljs-utils.ts
@@ -4,9 +4,11 @@ export class ShelljsUtils {
 
   static safeExec(command: string, errorMessage?: string): ShellString {
     const result = shelljs.exec(command);
-    if (result.code !== 0) {
-      throw new Error(errorMessage ?? 'Command returned non 0 error code');
+    const exitCode = result.code;
+    if (exitCode === 0) {
+      return result;
+    } else {
+      throw new Error(errorMessage ?? `Command returned with ${exitCode} exit code`);
     }
-    return result;
   }
 }

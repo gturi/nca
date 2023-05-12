ee4924785ecfedafd5b5df569eeff875fdbef1c2
gventurini <giacomo.venturini3@gmail.com>
2023-05-13 00:21:31 +0200

    add log-positional command tests

---
+++ test/command-handler/function/positional/param.test.js
@@ -0,0 +1,26 @@
+const { spawn } = require('child_process');
+const testUtils = require('../../../utils/test-utils')
+
+describe("function command handler", () => {
+
+  beforeEach(() => jest.resetModules());
+
+  afterEach(() => jest.resetAllMocks());
+
+  it('log-positional command ignores boolean positional argument when not specified', done => {
+    const expected = "false\n1\n[ 'a', 'b' ]\n";
+    const handleResult = output => {
+      expect(output.join('')).toBe(expected);
+    };
+    testUtils.runNcaAndVerifyOutput(done, handleResult, 'log-positional 1 a b');
+  });
+
+  it('log-positional command accepts --{key}={value} syntax', done => {
+    const expected = "true\n1\n[ 'a', 'b' ]\n";
+    const command = 'log-positional --foo=true 1 a b';
+    const handleResult = output => {
+      expect(output.join('')).toBe(expected);
+    };
+    testUtils.runNcaAndVerifyOutput(done, handleResult, command);
+  });
+});

4cafbc66d8cded99c7191d410979c1acbe9940be
gventurini <giacomo.venturini3@gmail.com>
2023-05-13 00:28:27 +0200

    add shell js test

---
+++ test/command-handler/function/shell-js/shell-js.test.js
@@ -0,0 +1,17 @@
+const { spawn } = require('child_process');
+const testUtils = require('../../../utils/test-utils')
+
+describe("function command handler", () => {
+
+  beforeEach(() => jest.resetModules());
+
+  afterEach(() => jest.resetAllMocks());
+
+  it('function-shell-js command logs hello world via shell js', done => {
+    const expected = "hello world\n";
+    const handleResult = output => {
+      expect(output.join('')).toBe(expected);
+    };
+    testUtils.runNcaAndVerifyOutput(done, handleResult, 'function-shell-js');
+  });
+});

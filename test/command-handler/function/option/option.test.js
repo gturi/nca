54dd12d115a0c333a7744e39a4b6d10ac1f1cfb7
gventurini <giacomo.venturini3@gmail.com>
2023-05-12 23:10:18 +0200

    add log-options test

---
+++ test/command-handler/function/option/option.test.js
@@ -0,0 +1,25 @@
+const testUtils = require('../../../utils/test-utils')
+
+describe("function command handler", () => {
+
+  beforeEach(() => jest.resetModules());
+
+  afterEach(() => jest.resetAllMocks());
+
+  it('log-options command logs default values if no option param is specified', done => {
+    const expected = 'false 1 foo\n';
+    const handleResult = output => {
+      expect(output.join('\n')).toBe(expected);
+    };
+    testUtils.runNcaAndVerifyOutput(done, handleResult, 'log-options');
+  });
+
+  /*it('log-options command logs the specified input values', done => {
+    const expected = [true, 100, 'bar'].join(' ') + '\n';
+    const command = ['log-options', '-b', 'true', '-n', '100', '-s', 'bar'].join(' ');
+    const handleResult = output => {
+      expect(output.join('\n')).toBe(expected);
+    };
+    testUtils.runNcaAndVerifyOutput(done, handleResult, command);
+  });*/
+});

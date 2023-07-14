0f255b201b81b07439c51ce523ed1fefadd5116e
gventurini <giacomo.venturini3@gmail.com>
2023-07-15 00:40:55 +0200

    re add isEmpty function

--- src/util/string-utils.ts
+++ src/util/string-utils.ts
@@ -1,6 +1,11 @@
 export class StringUtils {
+
   static hasWhitespace(str: string): boolean {
     return /\s/.test(str);
+  }
+
+  static isEmpty(str: (string | undefined | null)): boolean {
+    return str === undefined || str === null || str === '';
   }
 
   static ifNotEmptyOrDefault(str: (string | undefined | null), getDefault: () => string): string {

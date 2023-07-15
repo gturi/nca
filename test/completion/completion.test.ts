b4445939bde93438dcaf30bccbff314273be2a47
gventurini <giacomo.venturini3@gmail.com>
2023-07-15 16:45:02 +0200

    add hierarchy default completion test

--- test/completion/completion.test.ts
+++ test/completion/completion.test.ts
@@ -38,4 +38,45 @@ describe("completion", () => {
 
     testUtils.runNcaAndVerifyOutput(verifyOutput, ...command);
   });
+
+  it('hierarchy default completion logs expected values', done => {
+    const command = [
+      '--get-yargs-completions',
+      'hierarchy-completion'
+    ];
+    const expected = [
+      'subAlias',
+      'anotherSubAlias'
+    ].join('\n') + '\n';
+    const verifyOutput = new VerifyOutputBuilder(done)
+      .handleStdout(stdout => expect(stdout.join('')).to.equal(expected))
+      .build();
+
+    testUtils.runNcaAndVerifyOutput(verifyOutput, ...command);
+  });
+
+  it('hierarchy default completion logs expected option values', done => {
+    const command = [
+      '--get-yargs-completions',
+      'hierarchy-completion',
+      '-'
+    ];
+    const expected = [
+      '-a',
+      '--help',
+      '-n',
+      '--version',
+      ''
+    ].join('\n').split('\n').sort();
+    const verifyOutput = new VerifyOutputBuilder(done)
+      .handleStdout(stdout => {
+        const result = stdout.join('').split('\n').sort();
+        //console.log(`result ${JSON.stringify(result)}`)
+        //console.log(`expected ${JSON.stringify(expected)}`)
+        expect(result).deep.equal(expected);
+      })
+      .build();
+
+    testUtils.runNcaAndVerifyOutput(verifyOutput, ...command);
+  });
 });

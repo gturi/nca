c8aa6f160bdadbbd0ce0b329dd87653140713b7b
gventurini <giacomo.venturini3@gmail.com>
2023-07-16 10:38:24 +0200

    add custom completion test

--- test/completion/completion.test.ts
+++ test/completion/completion.test.ts
@@ -67,7 +67,7 @@ describe("completion", () => {
       '-n',
       '--version',
       ''
-    ].join('\n').split('\n').sort();
+    ].sort();
     const verifyOutput = new VerifyOutputBuilder(done)
       .handleStdout(stdout => {
         const result = stdout.join('').split('\n').sort();
@@ -94,7 +94,7 @@ describe("completion", () => {
       '-n',
       '--version',
       ''
-    ].join('\n').split('\n').sort();
+    ].sort();
     const verifyOutput = new VerifyOutputBuilder(done)
       .handleStdout(stdout => {
         const result = stdout.join('').split('\n').sort();
@@ -121,7 +121,53 @@ describe("completion", () => {
       '-n',
       '--version',
       ''
-    ].join('\n').split('\n').sort();
+    ].sort();
+    const verifyOutput = new VerifyOutputBuilder(done)
+      .handleStdout(stdout => {
+        const result = stdout.join('').split('\n').sort();
+        //console.log(`result ${JSON.stringify(result)}`)
+        //console.log(`expected ${JSON.stringify(expected)}`)
+        expect(result).deep.equal(expected);
+      })
+      .build();
+
+    testUtils.runNcaAndVerifyOutput(verifyOutput, ...command);
+  });
+
+  it('hierarchy custom completion logs expected values', done => {
+    const command = [
+      '--get-yargs-completions',
+      'hierarchy-custom-completion'
+    ];
+    const expected = [
+      'firstValue',
+      'secondValue',
+      ''
+    ].sort();
+    const verifyOutput = new VerifyOutputBuilder(done)
+      .handleStdout(stdout => {
+        const result = stdout.join('').split('\n').sort();
+        //console.log(`result ${JSON.stringify(result)}`)
+        //console.log(`expected ${JSON.stringify(expected)}`)
+        expect(result).deep.equal(expected);
+      })
+      .build();
+
+    testUtils.runNcaAndVerifyOutput(verifyOutput, ...command);
+  });
+
+  it('hierarchy custom completion merge logs expected values', done => {
+    const command = [
+      '--get-yargs-completions',
+      'hierarchy-custom-completion-merge'
+    ];
+    const expected = [
+      'firstValue',
+      'secondValue',
+      'subAlias',
+      'anotherSubAlias',
+      ''
+    ].sort();
     const verifyOutput = new VerifyOutputBuilder(done)
       .handleStdout(stdout => {
         const result = stdout.join('').split('\n').sort();

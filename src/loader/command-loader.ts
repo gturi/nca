535ae0a84e78ecb3598697b814c372f2b2e9e8df
gventurini <giacomo.venturini3@gmail.com>
2024-02-19 18:04:25 +0100

    fix generic type inference error

--- src/loader/command-loader.ts
+++ src/loader/command-loader.ts
@@ -5,20 +5,21 @@ import { CompletionLoader } from "../mapper/completion-loader";
 import { ConfigLoader } from "./config-loader";
 import { iter } from "iterator-helper";
 import { CommandMapper } from "../mapper/command-mapper";
+import { AnyObj } from "../util/custom-types";
 
 export class CommandLoader {
 
-  initYargsCommands(argvBuilder: yargs.Argv<{}>) {
+  initYargsCommands<T = AnyObj>(argvBuilder: yargs.Argv<T>) {
     const configLoader = new ConfigLoader();
     const aliases = configLoader.loadAliases();
 
     iter(aliases.sort((a, b) => a.name.localeCompare(b.name)))
-      .map(alias => AliasMapper.map(alias))
+      .map(alias => AliasMapper.map<T>(alias))
       .forEach(commandModule => argvBuilder.command(commandModule));
 
     const builtInCommands = new BuiltInCommands();
     iter(builtInCommands.get())
-      .map(command => CommandMapper.map(command))
+      .map(command => CommandMapper.map<T>(command))
       .forEach(commandModule => argvBuilder.command(commandModule));
 
     CompletionLoader.initCompletion(argvBuilder, aliases);

bd902cc9e338066fe29c209d45c12ba0587f595e
gventurini <giacomo.venturini3@gmail.com>
2024-03-03 12:06:10 +0100

    fix test failing on windows

--- test/integration/command-handler/module/iterator-helper/module-iterator-helper.test.ts
+++ test/integration/command-handler/module/iterator-helper/module-iterator-helper.test.ts
@@ -2,14 +2,13 @@ import 'mocha';
 import { expect } from 'chai';
 import * as testUtils from '../../../../utils/test-utils';
 import { VerifyOutputBuilder } from '../../../../utils/verify-output-builder';
-import { Platform } from '../../../../utils/platform-values/platform';
 
 describe("module command handler", () => {
 
 
   it('command module-iterator-helper command even values using a lazy iterator', done => {
     const command = 'module-iterator-helper';
-    const expected = Platform.addNewLine([2, 4, 6].join(Platform.values.newLine));
+    const expected = [2, 4, 6].join('\n') + '\n';
     const verifyOutput = new VerifyOutputBuilder(done)
       .handleStdout((stdout: string[]) => {
         expect(stdout.join('')).to.equal(expected);

4ee908e5805b6ecfcc9d2cbb53f2e155167075d3
gventurini <giacomo.venturini3@gmail.com>
2024-03-03 11:38:49 +0100

    fix test description

--- test/integration/command-handler/module/external-dependency/external-dependency.test.ts
+++ test/integration/command-handler/module/external-dependency/external-dependency.test.ts
@@ -6,7 +6,7 @@ import { VerifyOutputBuilder } from '../../../../utils/verify-output-builder';
 describe("module command handler", () => {
 
 
-  it('command module-external logs todays date using moment.js library', done => {
+  it('command module-external performs aggregation using lodash library', done => {
     const command = 'module-external';
     const expected = `[[1,3],[2,4]]\n`;
     const verifyOutput = new VerifyOutputBuilder(done)

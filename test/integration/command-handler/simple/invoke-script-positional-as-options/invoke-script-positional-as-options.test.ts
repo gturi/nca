617ef3f69078c5f95df893bdf0002e4329465830
gventurini <giacomo.venturini3@gmail.com>
2024-03-17 16:41:47 +0100

    add tests

---
+++ test/integration/command-handler/simple/invoke-script-positional-as-options/invoke-script-positional-as-options.test.ts
@@ -0,0 +1,48 @@
+import 'mocha';
+import { expect } from 'chai';
+import * as testUtils from '../../../../utils/test-utils';
+import { VerifyOutputBuilder } from '../../../../utils/verify-output-builder';
+import { Platform } from '../../../../utils/platform-values/platform';
+
+describe("simple command handler", () => {
+
+  if (Platform.isWindows()) {
+    console.log('Skipping *nix specific test');
+    return;
+  }
+
+  const testCases: { params: string[], expectedOutput: string }[] = [
+    {
+      params: ['-a', 'hello', '-s', 'world'],
+      expectedOutput: '-a hello -s world --foo fooValue'
+    },
+    {
+      params: ['-a', 'hello', '-s', 'world', 'someValue'],
+      expectedOutput: '-a hello -s world --foo someValue'
+    },
+    {
+      params: ['-a', 'hello', '-s', 'world', 'someValue', 'anotherValue'],
+      expectedOutput: '-a hello -s world --foo someValue,anotherValue'
+    },
+    {
+      params: ['-a', 'hello', '-s', 'world', 'someValue,anotherValue'],
+      expectedOutput: '-a hello -s world --foo someValue,anotherValue'
+    },
+    {
+      params: ['-a', '"hel lo"', '-s', 'world', '"someValue anotherValue"'],
+      expectedOutput: '-a hel lo -s world --foo someValue anotherValue'
+    }
+  ];
+
+  testCases.forEach((testCase, index) => {
+    it(`command params logs the input params (idx: ${index})`, done => {
+      const verifyOutput = new VerifyOutputBuilder(done)
+        .handleStdout((stdout: string[]) => {
+          expect(stdout.join('\n')).to.equal(Platform.addNewLine(testCase.expectedOutput));
+        })
+        .build();
+      testUtils.runNcaAndVerifyOutput(verifyOutput, 'invoke-script-v2', ...testCase.params);
+    });
+  });
+
+});

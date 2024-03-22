e7702e6950e544ac29b9b569b6aa4824cde2bbc8
gventurini <giacomo.venturini3@gmail.com>
2024-03-22 21:41:08 +0100

    add export of command handler input and completion input

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { YargsLoader } from './loader/yargs-loader';
+import { CommandHandlerInput } from './model/input/command-handler-input';
+import { CompletionInput } from './model/input/completion-input';
+
 
 function nca() {
   const yargsLoader = new YargsLoader();
   yargsLoader.init();
 }
 
-export { nca };
+export {
+  nca,
+  CommandHandlerInput,
+  CompletionInput
+}
